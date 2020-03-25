9463b7d0135ed1b35d2cbf1f64e73fb634fcae34
Maria Paderina <mariapaderina79@gmail.com>
2020-03-24 19:55:36 -0700

    fixed test

--- src/app/task-add/task-add.component.spec.ts
+++ src/app/task-add/task-add.component.spec.ts
@@ -18,8 +18,4 @@ describe('TaskAddComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/task/task.component.spec.ts
+++ src/app/task/task.component.spec.ts
@@ -18,8 +18,4 @@ describe('TaskComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
